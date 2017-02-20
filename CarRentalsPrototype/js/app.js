04dedcf552ee73d6c92595b513c2ac898698ac54
unknown <igaisiuk@IGaisiuk.kiev.luxoft.com>
2017-02-20 19:04:30 +0200

    AUS-LAX WebExtanded & mWebCollapsible

--- CarRentalsPrototype/js/app.js
+++ CarRentalsPrototype/js/app.js
@@ -1,3 +1,10 @@
 $(document).foundation();
 
-$('.reveal-modal.small.center').css('color', 'red');
+//$('.reveal-modal.small.center').css('color', 'red');
+
+//$("#Collapse").click(function(){
+//    $("#Collapse i").toggleClass("arrow-down-2").toggleClass("arrow-up-2");
+//    $(".AirportList, .AirportCarProperties").slideToggle(function(){
+//        //$(".Collapse").css('margin-top', '15px');
+//    });
+//});
