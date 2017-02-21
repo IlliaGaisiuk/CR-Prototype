30d67c04ba9c83b55ae77173774d24bcffc8d5f8
unknown <igaisiuk@IGaisiuk.kiev.luxoft.com>
2017-02-21 17:33:29 +0200

    gulp set up 1

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,7 @@ gulp.task('sass', function () {
         .pipe($.autoprefixer({
             browsers: ['last 2 versions', 'ie >= 9']
         }))
-        .pipe(gulp.dest('app.css'));
+        .pipe(gulp.dest('./css/app.css'));
 });
 
 gulp.task('default', ['sass', 'server']);
